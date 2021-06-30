8cfc0b41806f28e6648147cb7859b3a01270d611
aristrisnawan <aristrisnawan35@gmail.com>
2021-06-30 17:38:09 +0800

    Add Page About Us

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-12 text-center mb-5 text-info">
+        <h2><strong>Sejarah Perusahaan</strong></h2>
+      </div>
+      <div class="col-md-6">
+        <p>
+          BatuGajah merupakan perusahaan yang bergerak di bidang konveksi.
+          Pereusahaan ini didirikan dan dikeolal oleh keluarga bapak Abdul. Saat
+          ini bapak Abdul selaku penggagas berprovesi sebagai pemilik. BatuGajah
+          juga menerima pesanan bersekala besar dan juga pemesanan yang
+          bersekala kecil
+        </p>
+      </div>
+      <div class="col-md-6">
+        <p>
+          BatuGajah berdiri pada tanggal 25 Agustus 1998. Sempat beberapa kali
+          berganti nama, pada awal berdirinya BatuGajah ini bernama PT Aki Domba
+          kemudian pada tahun 2000 berganti lagi menjadi PT Aki Jimu, setelah
+          berjalan 3 tahun berganti lagi menjadi BatuGajah dan masih bertahan
+          sampai sekarang. Nomo Surat Izin Usaha Perdagangan (SIUP):
+          33/343/1998.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center text-info mt-5 mb-5">
+        <h2><strong>Visi Misi</strong></h2>
+      </div>
+      <div class="col-md-6">
+        <h3><strong>Visi</strong></h3>
+        <p>Menjadi perusahaan yang terdepan dalam :</p>
+        <ol>
+          <li>Menjadi penyedia pakaian terpercaya di Indonesia.</li>
+          <li>Menjadi perusahaan yang ramah lingkungan.</li>
+          <li>
+            Menjadi perusahaan konveksi yang mempunyai daya saing dan terkemuka
+            di daerah dan di luar daerah.
+          </li>
+        </ol>
+      </div>
+      <div class="col-md-6">
+        <h3><strong>Misi</strong></h3>
+        <ol>
+          <li>Menyediakan pakaian yang merupakan produk terbaik.</li>
+          <li>Memberikan pelayanan terbaik pada konsumen.</li>
+          <li>Menyediakan pakaian yang merupakan produk terbaik.</li>
+          <li>Memberikan pelayanan terbaik pada konsumen.</li>
+        </ol>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center text-info mt-5 mb-5">
+        <h2><strong>Team Kami</strong></h2>
+      </div>
+      <div class="col-md-4">
+        <div class="card-group">
+          <div class="card">
+            <img class="card-img-top team mx-auto d-block rounded-circle align-enter" src="../assets/images/abdul.jpg" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style>
+</style>
--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="light" class="shadow bg-white fixed-top scrolling-navbar">
+    <b-navbar toggleable="lg" type="light" class="shadow bg-white  scrolling-navbar">
       <div class="container">
         <router-link class="navbar-brand" to="/"><h2>BatuGajah</h2></router-link>
 
@@ -21,7 +21,7 @@
               <router-link class="nav-link" to=""><h5>Testimoni</h5></router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to=""><h5>About Us</h5></router-link>
+              <router-link class="nav-link" to="/About"><h5>About Us</h5></router-link>
             </li>
           </b-navbar-nav>
           </b-navbar-nav>
